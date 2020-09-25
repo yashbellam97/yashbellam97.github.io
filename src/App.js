47a52109ad99adbada416d1e3960d674cd0aefe9
yashbellam97 <yashbellam97@gmail.com>
2020-09-25 13:50:20 -0400

    Add projects wrapper

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import "./App.scss"
 
 import HeroWrapper from "./components/HeroWrapper"
+import ProjectsWrapper from "./components/ProjectsWrapper"
 
 function App() {
     return (
         <div className="container">
             <HeroWrapper />
+            <ProjectsWrapper />
         </div>
     );
 }
---
+++ src/components/ProjectsWrapper.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProjectsWrapper() {
+    return (
+        <div className="projects-wrapper">
+            <h1>Hello, world</h1>
+        </div>
+    );
+}
+
+export default ProjectsWrapper;

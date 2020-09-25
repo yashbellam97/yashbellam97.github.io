436df2ad69cfa3d0e87cfe4afb365794993d9076
yashbellam97 <yashbellam97@gmail.com>
2020-09-24 21:18:45 -0400

    Add Intro section

--- src/components/HeroWrapper.js
+++ src/components/HeroWrapper.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Wrapper from "./Wrapper"
 import Header from "./Header"
+import Intro from "./Intro"
 
 function HeroWrapper() {
     return (
         <div className="hero-wrapper">
             <Wrapper>
                 <Header />
+                <Intro />
             </Wrapper>
         </div>
     );
---
+++ src/components/Intro.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Intro() {
+    return (
+        <div className="intro">
+            <h1>Hello world, this is Yaswanth.</h1>
+            <p className="subtitle">I am a full stack developer</p>
+        </div>
+    );
+}
+
+export default Intro;

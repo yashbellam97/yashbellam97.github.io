f9c373124c57a9baf6e4601ef271a37d603a45a5
yashbellam97 <yashbellam97@gmail.com>
2020-09-26 20:17:43 -0400

    Add arrows to scroll projects

--- src/components/ProjectsWrapper.js
+++ src/components/ProjectsWrapper.js
@@ -2,11 +2,14 @@ import React from "react";
 
 import Wrapper from "./Wrapper";
 
+import arrow from "../images/arrow-icon.svg"
+
 function ProjectsWrapper() {
     return (
         <div className="projects-wrapper">
-            <Wrapper className="extended-wrapper">
-                <h1>Hello, world</h1>
+            <Wrapper className={["extended-wrapper", "carousel-container"].join(" ")}>
+                <img src={arrow} id="left-arrow" className="arrow" alt="Move project cards to the right"/>
+                <img src={arrow} id="right-arrow" className="arrow" alt="Move project cards to the left"/>
             </Wrapper>
         </div>
     );

218725b2e315dbbcfc3b25a0cf3d2bd01c0f2942
yashbellam97 <yashbellam97@gmail.com>
2020-09-27 00:11:16 -0400

    Implement project card UI

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,8 +1,33 @@
 import React from "react";
 
-function ProjectCard() {
+import gitHubLogo from "../images/logos/github-logo.svg"
+import globeLogo from "../images/logos/globe-logo.svg"
+
+function ProjectCard(props) {
+    const technologyList = props.projectDetails.technologies.map((technology) => <li><img src={technology.technologyLogo} alt={technology.technologyName + " logo"} /></li>);
+    let urlList = [];
+    if ("liveUrl" in props.projectDetails.urls) {
+        urlList.push(<li><a href={props.projectDetails.urls.liveUrl}><img src={globeLogo} alt="Project website logo"/></a></li>);
+    }
+    if ("githubUrl" in props.projectDetails.urls) {
+        urlList.push(<li><a href={props.projectDetails.urls.githubUrl}><img src={gitHubLogo} alt="Project GitHub logo"/></a></li>);
+    }
+
     return (
-        <h1>Hello, world</h1>
+        <div className="project-card">
+            <div className="project-title">
+                <ul>
+                    {technologyList}
+                </ul>
+                <h2>{props.projectDetails.projectName}</h2>
+            </div>
+            <div className="project-details">
+                <p>{props.projectDetails.projectDescription}</p>
+                <ul>
+                    {urlList}
+                </ul>
+            </div>
+        </div>
     );
 }
 

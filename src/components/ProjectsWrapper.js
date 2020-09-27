9461af472fe8e2643a84d5ee2ee65fabe7e064cd
yashbellam97 <yashbellam97@gmail.com>
2020-09-27 00:11:52 -0400

    Populate carousel with project cards

--- src/components/ProjectsWrapper.js
+++ src/components/ProjectsWrapper.js
@@ -5,16 +5,25 @@ import ProjectCard from "./ProjectCard"
 
 import arrow from "../images/arrow-icon.svg"
 
+import projectsData from "../projectsData"
+
 function ProjectsWrapper() {
+    const projectList = projectListGenerator();
     return (
         <div className="projects-wrapper">
             <Wrapper className={["extended-wrapper", "carousel-container"].join(" ")}>
                 <img src={arrow} id="left-arrow" className="arrow" alt="Move project cards to the right"/>
-                <ProjectCard />
+                <ul className="carousel">
+                    {projectList}
+                </ul>
                 <img src={arrow} id="right-arrow" className="arrow" alt="Move project cards to the left"/>
             </Wrapper>
         </div>
     );
+}
+
+function projectListGenerator() {
+    return projectsData.map((project) => <ProjectCard projectDetails={project} key={project.projectId} />);
 }
 
 export default ProjectsWrapper;

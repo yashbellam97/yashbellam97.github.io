60a3e3cce035c60d9dc4e9ae2cd94328bd77161f
yashbellam97 <yashbellam97@gmail.com>
2020-10-04 19:39:51 -0400

    Add skills wrapper

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./App.scss"
 
 import HeroWrapper from "./components/HeroWrapper"
 import ProjectsWrapper from "./components/ProjectsWrapper"
+import SkillsWrapper from "./components/SkillsWrapper"
 
 function App() {
     return (
         <div className="container">
             <HeroWrapper />
             <ProjectsWrapper />
+            <SkillsWrapper />
         </div>
     );
 }

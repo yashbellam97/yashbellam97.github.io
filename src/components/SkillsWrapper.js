c8dffb6a54c1b0fe00d9cb41b5891d9c37d93ba8
yashbellam97 <yashbellam97@gmail.com>
2020-10-04 20:47:31 -0400

    Add skills to SkillsWrapper

--- src/components/SkillsWrapper.js
+++ src/components/SkillsWrapper.js
@@ -1,9 +1,25 @@
 import React from "react";
 
+import Wrapper from "./Wrapper";
+
+import skillsData from "../skillsData";
+
 function SkillsWrapper() {
+    const skillList = skillListGenerator();
     return (
-        <h1>Hello, world</h1>
+        <div className="skills-wrapper">
+            <Wrapper className="wrapper">
+                <h2>Skills</h2>
+                <ul className="skills">
+                    {skillList}
+                </ul>
+            </Wrapper>
+        </div>
     );
+}
+
+function skillListGenerator() {
+    return skillsData.map((skill) => <li><img src={skill.skillLogo} alt={skill.skillName + " logo"} /></li>);
 }
 
 export default SkillsWrapper;

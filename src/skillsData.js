e9d7427a1fde513c2cd8034cca5e6a9baa60d4d1
yashbellam97 <yashbellam97@gmail.com>
2020-10-04 20:37:03 -0400

    Add skill data file

---
+++ src/skillsData.js
@@ -0,0 +1,95 @@
+import androidLogo from "./images/logos/android-logo.svg";
+import cLogo from "./images/logos/c-logo.svg";
+import cSharpLogo from "./images/logos/c-sharp-logo.svg";
+import cssLogo from "./images/logos/css-logo.svg";
+import databaseLogo from "./images/logos/database-logo.svg";
+import elixirLogo from "./images/logos/elixir-logo.svg";
+import gitLogo from "./images/logos/git-logo.svg"
+import htmlLogo from "./images/logos/html-logo.svg";
+import javaLogo from "./images/logos/java-logo.svg";
+import javascriptLogo from "./images/logos/javascript-logo.svg";
+import kotlinLogo from "./images/logos/kotlin-logo.svg";
+import phpLogo from "./images/logos/php-logo.svg";
+import pythonLogo from "./images/logos/python-logo.svg";
+import reactLogo from "./images/logos/react-logo.svg";
+import springBootLogo from "./images/logos/spring-boot-logo.svg";
+
+const skillsData = [
+    {
+        skillId: 0,
+        skillName: "Java",
+        skillLogo: javaLogo
+    },
+    {
+        skillId: 1,
+        skillName: "JavaScript",
+        skillLogo: javascriptLogo
+    },
+    {
+        skillId: 2,
+        skillName: "HTML",
+        skillLogo: htmlLogo
+    },
+    {
+        skillId: 3,
+        skillName: "CSS",
+        skillLogo: cssLogo
+    },
+    {
+        skillId: 4,
+        skillName: "Android",
+        skillLogo: androidLogo
+    },
+    {
+        skillId: 5,
+        skillName: "Git",
+        skillLogo: gitLogo
+    },
+    {
+        skillId: 6,
+        skillName: "React",
+        skillLogo: reactLogo
+    },
+    {
+        skillId: 7,
+        skillName: "Spring Boot",
+        skillLogo: springBootLogo
+    },
+    {
+        skillId: 8,
+        skillName: "Python",
+        skillLogo: pythonLogo
+    },
+    {
+        skillId: 9,
+        skillName: "C#",
+        skillLogo: cSharpLogo
+    },
+    {
+        skillId: 10,
+        skillName: "SQL",
+        skillLogo: databaseLogo
+    },
+    {
+        skillId: 11,
+        skillName: "PHP",
+        skillLogo: phpLogo
+    },
+    {
+        skillId: 12,
+        skillName: "Kotlin",
+        skillLogo: kotlinLogo
+    },
+    {
+        skillId: 13,
+        skillName: "Elixir",
+        skillLogo: elixirLogo
+    },
+    {
+        skillId: 14,
+        skillName: "C",
+        skillLogo: cLogo
+    }
+]
+
+export default skillsData;

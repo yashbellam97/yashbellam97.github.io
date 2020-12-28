60699d493c9f91cdc6244ea738df36bd5861af75
yashbellam97 <yashbellam97@gmail.com>
2020-12-28 16:00:29 -0500

    Add tooltips to ProjectCards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,16 +1,42 @@
 import React from "react";
 
+import Tooltip from '@material-ui/core/Tooltip';
+
 import gitHubLogo from "../images/logos/github-logo.svg"
 import globeLogo from "../images/logos/globe-logo.svg"
 
 function ProjectCard(props) {
-    const technologyList = props.projectDetails.technologies.map((technology) => <li key={technology.technologyName}><img src={technology.technologyLogo} alt={technology.technologyName + " logo"} /></li>);
+    const technologyList = props.projectDetails.technologies.map((technology) =>
+        <li key={technology.technologyName}>
+            <Tooltip title={technology.technologyName} placement="top" arrow>
+                <img src={technology.technologyLogo} alt={technology.technologyName + " logo"} />
+            </Tooltip>
+        </li>
+    );
+
     let urlList = [];
     if ("liveUrl" in props.projectDetails.urls) {
-        urlList.push(<li key="website"><a href={props.projectDetails.urls.liveUrl}><img src={globeLogo} alt="Project website logo" /></a></li>);
+        urlList.push(
+            <li key="website">
+                <Tooltip title="Visit website" placement="bottom" arrow>
+                    <a href={props.projectDetails.urls.liveUrl}>
+                        <img src={globeLogo} alt="Project website logo" />
+                    </a>
+                </Tooltip>
+            </li>
+        );
     }
+
     if ("githubUrl" in props.projectDetails.urls) {
-        urlList.push(<li key="github"><a href={props.projectDetails.urls.githubUrl}><img src={gitHubLogo} alt="Project GitHub logo" /></a></li>);
+        urlList.push(
+            <li key="github">
+                <Tooltip title="Visit GitHub" placement="bottom" arrow>
+                    <a href={props.projectDetails.urls.githubUrl}>
+                        <img src={gitHubLogo} alt="Project GitHub logo" />
+                    </a>
+                </Tooltip>
+            </li>
+        );
     }
 
     return (

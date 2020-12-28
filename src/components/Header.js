455697e64a3abd9e58fadb118413efc2def6ba49
yashbellam97 <yashbellam97@gmail.com>
2020-12-28 15:29:32 -0500

    Convert nav links to buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,26 @@
 import React from "react";
 
+import { ThemeProvider } from "@material-ui/styles";
+import Button from "@material-ui/core/Button";
+
+import { darkTheme } from "../materialThemes";
+import materialStyles from "../materialStyles";
+
 import logo from "../images/yb-logo.svg"
 
 function Header() {
+    const classes = materialStyles();
+
     return (
         <header>
             <a href="#" className="logo">
                 <img id="yb-logo" src={logo} alt="YB logo" />
             </a>
-            <nav>
-                <ul>
-                    <li><a href="#projects">Projects</a></li>
-                    <li><a href="#">Resume</a></li>
-                </ul>
+            <nav className={classes.root}>
+                <ThemeProvider theme={darkTheme}>
+                    <Button href="#projects" size="small">Projects</Button>
+                    <Button href="#" variant="contained" size="small" color="primary">Resume</Button>
+                </ThemeProvider>
             </nav>
         </header>
     );

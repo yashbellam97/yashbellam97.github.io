55f69bc8dfa67cf9ac269f7c1fced2cef4573aea
yashbellam97 <yashbellam97@gmail.com>
2020-12-29 21:49:12 -0500

    Move Header outside HeroWrapper

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import "./App.scss"
 
+import Header from "./components/Header"
 import HeroWrapper from "./components/HeroWrapper"
 import ProjectsWrapper from "./components/ProjectsWrapper"
 import SkillsWrapper from "./components/SkillsWrapper"
@@ -9,6 +10,7 @@ import SkillsWrapper from "./components/SkillsWrapper"
 function App() {
     return (
         <div className="container">
+            <Header />
             <HeroWrapper />
             <ProjectsWrapper />
             <SkillsWrapper />
--- src/components/HeroWrapper.js
+++ src/components/HeroWrapper.js
@@ -1,14 +1,12 @@
 import React from "react";
 
 import Wrapper from "./Wrapper"
-import Header from "./Header"
 import Intro from "./Intro"
 
 function HeroWrapper() {
     return (
         <div className="hero-wrapper">
             <Wrapper className="wrapper">
-                <Header />
                 <Intro />
             </Wrapper>
         </div>
